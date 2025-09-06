a5ede6c23ce1bc8d00eb3802e2d6c62aee570d1a
Piyush <pk1620826@gmail.com>
2025-09-06 10:40:20 +0530

    follow the comment

--- ugit/cli.py
+++ ugit/cli.py
@@ -3,3 +3,5 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+# run '$ python3 setup.py develop --user' in terminal to install the package in development mode and make the 'ugit' command available globally. now you can run ugit and see the output 'Hello World!' in the terminal.
