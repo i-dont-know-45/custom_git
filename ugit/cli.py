1cc1a9dc12721f17dde8f817a975f233bc81829e
Piyush <pk1620826@gmail.com>
2025-09-06 12:39:43 +0530

    created our first command init

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,7 +1,26 @@
+import argparse
+
 def main():
-    print("Hello World!")
-    
+    args = parse_args()
+    args.func(args)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="ugit: a simple git implementation")
+
+    command = parser.add_subparsers(dest="command", required=True)
+
+    init_parser = command.add_parser("init", help="Initialize a new ugit repository")
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def init(args):
+    print("Hello, World!")
+
+
 if __name__ == "__main__":
     main()
-
-# run '$ python3 setup.py develop --user' in terminal to install the package in development mode and make the 'ugit' command available globally. now you can run ugit and see the output 'Hello World!' in the terminal.
+    
+#created a command 'init' that prints 'Hello, World!' when you write 'ugit init' in the terminal
+    
