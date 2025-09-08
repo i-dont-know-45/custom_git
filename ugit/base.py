c98718f11575d1ba31fdb7f78c56c991e52458ab
Piyush <pk1620826@gmail.com>
2025-09-08 22:30:31 +0530

    added a ignored function to avoid adding .ugit directory

--- ugit/base.py
+++ ugit/base.py
@@ -5,10 +5,16 @@ def write_tree(directory='.'):
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
+            if ignored(full):
+                continue
             if entry.is_file(follow_symlinks=False):
                 print(full)
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
                 
-                
-# Recursively traverses directory tree, printing file paths. Does not follow symlinks.
+
+def ignored(path):
+    return '.ugit' in path.split('/')
+
+           
+# ignored() checks if path is inside '.ugit'
