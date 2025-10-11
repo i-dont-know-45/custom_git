4d663d5d770391b118302814940a7ffc14c81e46
Piyush <pk1620826@gmail.com>
2025-10-11 19:07:12 +0530

    added a new feature ugit log

--- ugit/base.py
+++ ugit/base.py
@@ -1,5 +1,8 @@
 from . import data
 import os
+from collections import namedtuple
+import itertools
+import operator
 
 
 def write_tree(directory="."):
@@ -74,13 +77,32 @@ def commit(message):
     commit = f"tree {write_tree()}\n"
     HEAD = data.get_HEAD()
     if HEAD:
-        commit+=f'parent {HEAD}\n'
-        
+        commit += f"parent {HEAD}\n"
+
     commit += "\n"
     commit += f"{message}\n"
     oid = data.hash_object(commit.encode(), "commit")
     data.set_HEAD(oid)
     return oid
+
+
+Commit = namedtuple("Commit", ["tree", "parent", "message"])
+
+
+def get_commit(oid):
+    parent = None
+    commit = data.get_object(oid, "commit").decode()
+    lines = iter(commit.splitlines())
+    for line in itertools.takewhile(operator.truth, lines):
+        key, value = line.split(" ", 1)
+        if key == "tree":
+            tree = value
+        elif key == "parent":
+            parent = value
+        else:
+            assert False, f"Unknown field {key}"
+    message = "\n".join(lines)
+    return Commit(tree=tree, parent=parent, message=message)
 
 
 def ignored(path):
--- ugit/cli.py
+++ ugit/cli.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import textwrap
 from . import data
 from . import base
 
@@ -46,6 +47,10 @@ def parse_args():
     commit_parser.set_defaults(func=commit)
     commit_parser.add_argument("-m", "--message", required=True)
 
+    log_parser = command.add_parser("log", help="Show commit logs")
+    log_parser.set_defaults(func=log)
+    log_parser.add_argument("oid", nargs="?")
+
     return parser.parse_args()
 
 
@@ -76,6 +81,16 @@ def read_tree(args):
 
 def commit(args):
     print(base.commit(args.message))
+
+
+def log(args):
+    oid = args.oid or data.get_HEAD()
+    while oid:
+        commit = base.get_commit(oid)
+        print(f"commit {oid}\n")
+        print(textwrap.indent(commit.message, "    "))
+        print("")
+        oid = commit.parent
 
 
 if __name__ == "__main__":
