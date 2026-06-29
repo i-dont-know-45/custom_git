99191fd6f2e04ea05081788de6f08b40eda8e7a4
Piyush <pk1620826@gmail.com>
2026-06-29 18:34:37 +0530

    allow git directory change

--- ugit/cli.py
+++ ugit/cli.py
@@ -9,8 +9,9 @@ import subprocess
 
 
 def main():
-    args = parse_args()
-    args.func(args)
+    with data.change_git_dir("."):
+        args = parse_args()
+        args.func(args)
 
 
 def parse_args():
--- ugit/data.py
+++ ugit/data.py
@@ -1,9 +1,17 @@
 import os
 import hashlib
 from collections import namedtuple
+from contextlib import contextmanager
 
-GIT_DIR = ".ugit"
+GIT_DIR = None
 
+@contextmanager
+def change_git_dir(new_dir):
+    global GIT_DIR
+    old_dir = GIT_DIR
+    GIT_DIR = f'{new_dir}/.ugit'
+    yield
+    GIT_DIR = old_dir
 
 def init():
     os.makedirs(GIT_DIR, exist_ok=True)
