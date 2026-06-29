574bab913de7bc957a0365d67361932ad4cd7c05
Piyush <pk1620826@gmail.com>
2026-06-29 20:55:35 +0530

    refactor(fetch): Retrieve remote refs in a separate function

--- ugit/remote.py
+++ ugit/remote.py
@@ -3,5 +3,9 @@ from . import data
 def fetch(remote_path):
     print ('Will fetch the following refs:')
     with data.change_git_dir(remote_path):
-        for refname,_ in data.iter_refs(prefix='refs/heads'):
+        for refname,_ in _get_remote_refs(remote_path,prefix='refs/heads').items():
             print(f'- {refname}')
+            
+def _get_remote_refs(remote_path,prefix=""):
+    with data.change_git_dir(remote_path):
+        return {refname: ref.value for refname,ref in data.iter_refs(prefix=prefix)}
